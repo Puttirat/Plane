ac64b02b7d5942b4c0b2b57b92dafd63a74b9a58
Puttirat <puttirat.lima@bumail.net>
2025-03-03 10:38:57 +0700

    create Code

---
+++ Assets/Scripts/AirPlane.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class AirPlane : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float liftBooster = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePower);
+        }
+    }
+}
